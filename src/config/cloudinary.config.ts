2293254f09611aa8039735dd3f01e95854319b99
assad <assadwazeer0@gmail.com>
2026-07-02 11:19:47 +0500

    File Uplod through cloudinary

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -4,9 +4,9 @@ export const CloudinaryConfig = {
   provide: 'CLOUDINARY',
   useFactory: () => {
     return cloudinary.config({
-      cloud_name: 'dxticujvp', 
-      api_key: '244949815878671',
-      api_secret: 'pu_NYofTDoak7_61jcZdIye50KM',
+      cloud_name: process.env.CLOUD_NAME, 
+      api_key: process.env.CLOUD_API_KEY,
+      api_secret: process.env.CLOUD_API_SECRET,
     });
   },
 };
